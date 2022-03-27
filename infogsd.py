8d8eea7133d184cbe9038f2e5c673f9640ee7ccb
Steven Lalewicz <steven.lalewicz@gmail.com>
2022-03-27 21:31:35 +0100

    Format registers normal and hex mode. Allow info cmd to display all registers. Add special registers to general.

--- infogsd.py
+++ infogsd.py
@@ -1,4 +1,9 @@
 from platform import machine
+
+GREEN = "\x1b[38;5;47m"
+WHITE = "\x1b[38;5;15m"
+RESET = "\x1b[0m"
+
 #-------------
 # register lists
 
@@ -8,7 +13,7 @@ r_list = {"r0": 0, "r1": 1, "r2": 2, "r3": 3, "r4": 4, "r5": 5, "r6": 6, "r7": 7
 x_list = {"x0": 0, "x1": 1, "x2": 2, "x3": 3, "x4": 4, "x5": 5, "x6": 6, "x7": 7, "x8": 8,
           "x9": 9, "x10": 10, "x11": 11, "x12": 12, "x13": 13, "x14": 14, "x15": 15, "x16": 16,
           "x17": 17, "x18": 18, "x19": 19, "x20": 20, "x21": 21, "x22": 22, "x23": 23, "x24": 24,
-          "x25": 25, "x26": 26, "x27": 27, "x28": 28, "x29": 29, "x30": 30}
+          "x25": 25, "x26": 26, "x27": 27, "x28": 28, "x29": 29, "x30": 30, 'sp': 31, 'pc': 32, 'cpsr': 33, 'fpsr': 34, 'fpcr': 35}
 
 s_list = {"s0": 0, "s1": 1, "s2": 2, "s3": 3, "s4": 4, "s5": 5, "s6": 6, "s7": 7, "s8": 8,
           "s9": 9, "s10": 10, "s11": 11, "s12": 12, "s13": 13, "s14": 14, "s15": 15, "s16": 16,
@@ -26,7 +31,6 @@ d_list = {"d0": 0, "d1": 1, "d2": 2, "d3": 3, "d4": 4, "d5": 5, "d6": 6, "d7": 7
 class InfoGSD(gdb.Command):
 
     def invoke(self, arguments, from_tty):
-        frame = gdb.selected_frame()
         argv = gdb.string_to_argv(arguments)
         
         list = []
@@ -51,10 +55,19 @@ class InfoGSD(gdb.Command):
             else:
                prev = name
                list.append(name)
+        else:
+            list = self.reglist
 
+        frame = gdb.selected_frame()
         for name in list:
             val = frame.read_register(name)
-            self.format_reg(name, val)
+            print(f'{GREEN}{name:<10}{RESET}{self.format_reg(val):<24} {self.format_reg_hex(val)}')
+
+    def format_reg(self, val):
+        return val.format_string()
+
+    def format_reg_hex(self, val):
+        return val.format_string(format='x')
 
 #---- general ----- 
 
@@ -66,9 +79,6 @@ class InfoGeneral64(InfoGSD):
 
     def __init__(self):
        super(InfoGeneral64, self).__init__("info general", gdb.COMMAND_DATA)
-
-    def format_reg(self, name, val):
-        print(name, val)
 
 class InfoGeneral32(InfoGSD):
     """info general Armv8-a"""
@@ -90,8 +100,13 @@ class InfoSingle64(InfoGSD):
     def __init__(self):
        super(InfoSingle64, self).__init__("info single", gdb.COMMAND_DATA)
 
-    def format_reg(self, name, val):
-        print(name, val['f'])
+    def format_reg(self, val):
+        return val['f'].format_string()
+
+    def format_reg_hex(self, val):
+        return val['u'].format_string(format='z')
+
+type_ptr_double = gdb.Value(0.0).type.pointer()
 
 class InfoSingle32(InfoGSD):
     """info single Armv8-a"""
@@ -101,6 +116,10 @@ class InfoSingle32(InfoGSD):
 
     def __init__(self):
        super(InfoSingle32, self).__init__("info single", gdb.COMMAND_DATA)
+
+
+    def format_reg_hex(self, val):
+        return val.cast(type_ptr_double).format_string(format="z")
 
 #---- double ----- 
 
@@ -113,8 +132,11 @@ class InfoDouble64(InfoGSD):
     def __init__(self):
        super(InfoDouble64, self).__init__("info double", gdb.COMMAND_DATA)
 
-    def format_reg(self, name, val):
-        print(name, val['f'])
+    def format_reg(self, val):
+        return val['f'].format_string()
+
+    def format_reg_hex(self, val):
+        return val['u'].format_string(format='x')
 
 class InfoDouble32(InfoGSD):
     """info double Armv8-a"""
@@ -126,8 +148,10 @@ class InfoDouble32(InfoGSD):
        super(InfoDouble32, self).__init__("info double", gdb.COMMAND_DATA)
 
     def format_reg(self, name, val):
-        print(name, val['f64'])
+        return val['f64'].format_string()
 
+    def format_reg_hex(self, val):
+        return val['u64'].format_string(format='x')
 
 if machine() == "aarch64":
     InfoGeneral64()
